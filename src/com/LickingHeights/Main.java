039b517c984a347888509d28fa9409ed443adc5b
Logan Keller <logan.keller.2023@student.lhschools.org>
2020-02-24 11:54:34 -0500

    IntelliJ commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,25 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+System.out.println("+--------------------------------------+");
+System.out.println("|        Shell Gas Station             |");
+System.out.println("|   Address  9749 Hazelton Etna Rd SW  |");
+System.out.println("|        Date        Time              |");
+System.out.println("|        1/9/2020    11:00 AM          |");
+System.out.println("|      Invoice Number#   134254        |");
+System.out.println("|          Auth#   101010              |");
+System.out.println("|            MasterCard                |");
+System.out.println("|          ************52              |");
+System.out.println("|                                      |");
+System.out.println("|      Pump     Product      $/G       |");
+System.out.println("|       03       Diesel       $3.05    |");
+System.out.println("|           Amount   Total             |");
+System.out.println("|          15       $45.75             |");
+System.out.println("|            Thank You!                |");
+System.out.println("|         Shell Gas Station            |");
+System.out.println("+--------------------------------------+");
+
+    }
+}
